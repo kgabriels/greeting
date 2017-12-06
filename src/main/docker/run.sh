744c4a86d16076ae0dc4a0bc466ee9a553f1aa52
kgabriels <krisgabriels@gmail.com>
2017-12-06 16:26:57 +0100

    prepared for docker

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo "Starting Greeting Server"
 echo "********************************************************"
-java -jar /usr/local/greetingservice/github-greeting-0.0.1-SNAPSHOT.jar
+java -jar /usr/local/greetingservice/@project.build.finalName@.jar
